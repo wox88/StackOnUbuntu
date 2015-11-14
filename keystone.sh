93eb3a74687bcfb27db6cecb433eb1cf0d7a0269
Ronis <wox88@qq.com>
2015-11-14 17:01:03 +0800

    install_keystone added.

--- keystone.sh
+++ keystone.sh
@@ -17,3 +17,19 @@ function install_keystone {
 	echo "manual" | sudo tee /etc/init/keystone.override
 	sudo apt-get install -y python-mysqldb memcached python-memcache keystone apache2 libapache2-mod-wsgi python-openstackclient
 }
+
+function config_keystone {
+
+}
+
+function config_mod-wsgi_keystone {
+	sudo cp $SCRIPT_PATH/wsgi-keystone.conf.template /etc/apache2/sites-available/wsgi-keystone.conf
+	sudo ln -s /etc/apache2/sites-available/wsgi-keystone.conf /etc/apache2/sites-enabled
+	sudo mkdir -p /var/www/cgi-bin/keystone
+	sudo cp $SCRIPT_PATH/keystone-wsgi-admin /var/www/cgi-bin/keystone/admin
+	sudo cp $SCRIPT_PATH/keystone-wsgi-public /var/www/cgi-bin/keystone/main
+	sudo chown -R keystone:keystone /var/www/cgi-bin/keystone
+	sudo chmod 755 /var/www/cgi-bin/keystone/*
+
+	sudo service apache2 restart
+}
